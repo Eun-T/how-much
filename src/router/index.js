1002e7107f62834f216cfd216e02e27e06ed4620
Euntae <venzire329@gmail.com>
2026-04-07 18:43:44 +0900

    Feat: 폴더 구조 변경

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/pages/Home.vue';
+import Profile from '@/pages/Profile.vue';
+import Statistic from '@/pages/Statistic.vue';
+import Travel from '@/pages/Travel.vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+  routes: [
+    { path: '/', component: Home },
+    { path: '/profile', component: Profile },
+    { path: '/statistic', component: Statistic },
+    { path: '/travel', component: Travel },
+  ],
+});
 
-export default router
+export default router;
